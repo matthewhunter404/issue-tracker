59910afee7bb21bcc7eeaf1eaf54e7437d5ecd2c
Matthew Hunter <matthewhunter404@gmail.com>
2024-03-01 22:52:58 +0200

    Add issues list view

--- issuetracker/issues/urls.py
+++ issuetracker/issues/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = "issues"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:issue_id>/", views.detail, name="detail"),
+    path("<int:issue_id>/comment/", views.comment, name="comment"),
+    path("<int:issue_id>/complete/", views.complete, name="complete"),
 ]
--- issuetracker/issues/views.py
+++ issuetracker/issues/views.py
@@ -1,4 +1,18 @@
 from django.http import HttpResponse
-
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
+from .models import Issue
 def index(request):
-    return HttpResponse("Hello, world. You're at the issues index.")
+    issue_list = Issue.objects.order_by("-creation_date")
+    context = {"issue_list": issue_list}
+    return render(request, "issues/index.html", context)
+
+def detail(request, issue_id):
+    issue = get_object_or_404(Issue, pk=issue_id)
+    return render(request, "issues/detail.html", {"issue": issue})
+
+def comment(request, issue_id):
+    return HttpResponse("You're adding a comment on issue %s." % issue_id)
+
+def complete(request, issue_id):
+    return HttpResponse("You're marking an issue as complete %s." % issue_id)
