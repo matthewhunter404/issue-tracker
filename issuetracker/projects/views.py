7b1b83ec340dabdf1c4b828e525f4c5e16b26d91
Matthew Hunter <matthewhunter404@gmail.com>
2024-06-05 14:04:02 +0200

    Add project views

--- issuetracker/projects/views.py
+++ issuetracker/projects/views.py
@@ -1,4 +1,11 @@
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from .models import Project
 
 def index(request):
-    return HttpResponse("Hello, world")
+    project_list = Project.objects.order_by("-creation_date")
+    context = {"project_list": project_list}
+    return render(request, "project/index.html", context)
+
+def detail(request, project_id):
+    issue = get_object_or_404(Project, pk=project_id)
+    return render(request, "project/detail.html", {"issue": issue})
